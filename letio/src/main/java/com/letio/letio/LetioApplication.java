73e7cb659ff1b7c04c6d65730e2358b017dc8d2a
brutalKim <132333588+brutalKim@users.noreply.github.com>
2024-02-15 20:37:24 +0900

    Add files via upload

---
+++ letio/src/main/java/com/letio/letio/LetioApplication.java
@@ -0,0 +1,13 @@
+package com.letio.letio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetioApplication.class, args);
+	}
+
+}
---
+++ letio/src/test/java/com/letio/letio/LetioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letio.letio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
